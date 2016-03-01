d1ae556833e03bd73353c6f2c2b1d6e932d9d24a
elisangeladayani <elisangeladayani@gmail.com>
2016-03-01 18:12:06 -0300

    Example de Configuration apache2

---
+++ webserver.rb
@@ -0,0 +1,14 @@
+package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
+#service 'apache2' do
+ # action [:stop, :disable]
+#end
